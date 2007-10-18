d9de6749fd6dee7e3c428c135aef47c19b34fb63
James Coglan <jcoglan@googlemail.com>
2007-10-18 19:24:32 +0000

    Removed unnecessary String#indexes method and implemented an improved implementation of JavaScript's String#replace method arguments. Thanks: http://termos.vemod.net/string-js_replace

--- lib/string.rb
+++ lib/string.rb
@@ -1,12 +1,4 @@
-require 'strscan'
-
 class String
-  def indexes(regexp)
-    scanner, ary = StringScanner.new(self), []
-    ary << scanner.pointer while scanner.scan_until(regexp)
-    ary
-  end
-  
   def rescape
     gsub(/([\/()[\]{}|*+-.,^$?\\])/, "\\\\1")
   end
