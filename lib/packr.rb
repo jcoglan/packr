4e2a10e1b7749a42ad42eeb76d6f1b8c2f58ad5b
James Coglan <jcoglan@googlemail.com>
2008-08-28 21:41:55 +0100

    Tidying up constants.

--- lib/packr.rb
+++ lib/packr.rb
@@ -26,11 +26,25 @@ class Packr
     "(OPERATOR)\\s*(REGEXP)" => "\\1\\2"
   })
   
-  class << self
-    def pack(script, options = {})
-      @packr ||= self.new
-      @packr.pack(script, options)
+  def self.encode62(c)
+    (c < 62 ? '' : encode62((c / 62.0).to_i)) +
+        ((c = c % 62) > 35 ? (c+29).chr : c.to_s(36))
+  end
+  
+  def self.encode52(c)
+    # Base52 encoding (a-Z)
+    encode = lambda do |d|
+      (d < 52 ? '' : encode.call((d / 52.0).to_i)) +
+          ((d = d % 52) > 25 ? (d + 39).chr : (d + 97).chr)
     end
+    encoded = encode.call(c.to_i)
+    encoded = encoded[1..-1] + '0' if encoded =~ /^(do|if|in)$/
+    encoded
+  end
+  
+  def self.pack(script, options = {})
+    @packr ||= self.new
+    @packr.pack(script, options)
   end
   
   def initialize
--- lib/packr/constants.rb
+++ lib/packr/constants.rb
@@ -1,30 +1,8 @@
 class Packr
   
   IGNORE  = RegexpGroup::IGNORE
-  KEYS    = "~"
   REMOVE  = ""
   SPACE   = " "
-  
-  CONTINUE = /\\\r?\n/
-  ENCODED  = /~\^(\d+)\^~/
-  PRIVATES = /\b_[\da-zA-Z$][\w$]*\b/
-  SHRUNK   = /@\d+\b/
-  
-  def self.encode62(c)
-    (c < 62 ? '' : encode62((c / 62.0).to_i)) +
-        ((c = c % 62) > 35 ? (c+29).chr : c.to_s(36))
-  end
-  
-  def self.encode52(c)
-    # Base52 encoding (a-Z)
-    encode = lambda do |d|
-      (d < 52 ? '' : encode.call((d / 52.0).to_i)) +
-          ((d = d % 52) > 25 ? (d + 39).chr : (d + 97).chr)
-    end
-    encoded = encode.call(c.to_i)
-    encoded = encoded[1..-1] + '0' if encoded =~ /^(do|if|in)$/
-    encoded
-  end
   
 end
 
