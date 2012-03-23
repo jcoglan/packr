59d6196b45d71ff3f56a7015b717dfdd5d9f2b12
James Coglan <jcoglan@gmail.com>
2012-03-23 21:52:40 +0000

    Change example to try out relative file paths.

--- example/compile.rb
+++ example/compile.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require File.expand_path('../../lib/packr', __FILE__)
+require 'fileutils'
 
 dir    = File.expand_path('..', __FILE__)
 code   = File.read(dir + '/example.js')
@@ -7,14 +8,16 @@ code   = File.read(dir + '/example.js')
 packed = Packr.pack(code,
   :shrink_vars    => true,
   :private        => true,
-  :source_files   => {'example.js' => 0},
+  :source_files   => {'../example.js' => 0},
   :generated_file => 'example-min.js'
 )
 
-File.open(dir + '/example-min.js', 'w') do |f|
+FileUtils.mkdir_p(dir + '/min')
+
+File.open(dir + '/min/example-min.js', 'w') do |f|
   f.write(packed)
 end
 
-File.open(dir + '/example-min.js.map', 'w') do |f|
+File.open(dir + '/min/example-min.js.map', 'w') do |f|
   f.write(packed.source_map.to_s)
 end
