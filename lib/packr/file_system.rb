4936ce84d07f2bc91b649c68c21577c14e0bbed5
James Coglan <jcoglan@gmail.com>
2012-03-24 02:03:04 +0000

    Move path expansion logic from SourceMap to FileSystem.

--- lib/packr/file_system.rb
+++ lib/packr/file_system.rb
@@ -13,7 +13,7 @@ class Packr
       offsets = {}
       
       sources.each do |source|
-        offsets[source] = code.size
+        offsets[relative_path(source, output)] = code.size
         code << File.read(source) + "\n"
       end
       
@@ -30,6 +30,18 @@ class Packr
       FileUtils.mkdir_p(File.dirname(output))
       File.open(output, 'w') { |f| f.write(packed) }
       File.open(source_map.filename, 'w') { |f| f.write(source_map.to_s) }
+    end
+    
+    def self.relative_path(source, target)
+      target_parts = target.split('/')
+      source_parts = source.split('/')
+      
+      while target_parts.first == source_parts.first
+        target_parts.shift
+        source_parts.shift
+      end
+      
+      ('../' * (target_parts.size-1)) + source_parts.join('/')
     end
     
   end
--- lib/packr/source_map.rb
+++ lib/packr/source_map.rb
@@ -12,11 +12,11 @@ class Packr
       @source_script  = script
       return unless options[:source_files]
       
-      @generated_file = File.expand_path(options[:output_file] || '.')
+      @generated_file = options[:output_file]
       @line_offset    = options[:line_offset] || 0
       
       @source_files = options[:source_files].
-                      map { |file, offset| [offset, relative_path(file)] }.
+                      map { |file, offset| [offset, file] }.
                       sort_by { |pair| pair.first }
       
       @source_files << [script.size, nil]
@@ -87,20 +87,6 @@ class Packr
     alias :to_s :to_json
     
   private
-    
-    def relative_path(path)
-      path = File.expand_path(path)
-      
-      target_parts = @generated_file.split('/')
-      source_parts = path.split('/')
-      
-      while target_parts.first == source_parts.first
-        target_parts.shift
-        source_parts.shift
-      end
-      
-      ('../' * (target_parts.size-1)) + source_parts.join('/')
-    end
     
     def tokenize(script)
       line_offsets = get_line_offsets(script)
