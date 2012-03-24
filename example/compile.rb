e0f7bba4e059dd50bcea69024c6c9af8ef7023ed
James Coglan <jcoglan@gmail.com>
2012-03-24 10:51:08 +0000

    Update the README.

--- example/compile.rb
+++ example/compile.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require File.expand_path('../../lib/packr', __FILE__)
-require 'fileutils'
 
 dir = File.expand_path('..', __FILE__)
 sources = ["#{dir}/example_a.js", "#{dir}/example_b.js"]
